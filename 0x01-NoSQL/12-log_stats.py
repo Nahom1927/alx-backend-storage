b1286e6abf487f57f401cadc6a97bb46a1fb3088
Nahom1927 <yheys898184@gmail.com>
2023-07-19 01:03:09 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,41 +1,12 @@
 #!/usr/bin/env python3
 """MongoDB storage"""
-from pymongo import MongoClient
+import pandas as pd
 
-
-if __name__ == "__main__":
-    """check for elements in  collection"""
-    client = MongoClient('mongodb://localhost:27017')
-    collection = client.logs.nginx
-
-    print(f"{collection.estimated_document_count()} logs")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
-
-    print("IPs:")
-    pipeline = [
-            {
-                '$group': {
-                    '_id': '$ip',
-                    'count': {'$sum': 1}
-                }
-            },
-            {
-                '$sort': {
-                    'count': -1
-                }
-            },
-            {
-                '$limit': 10
-            }
-    ]
-    result = collection.aggregate(pipeline)
-    for doc in result:
-        print(f"\t{doc['_id']}: {doc['count']}")
+df = pd.read_csv('access.log', sep=' ', header=None,
+                 names=['ip', 'dash1', 'dash2', 'time', 'request', 'status', 'size', 'referer', 'user_agent', 'forwarded_for'])
+df['time'] = df['time'].str.replace('[','').str.replace(']','')
+df['time'] = pd.to_datetime(df['time'], format='%d/%b/%Y:%H:%M:%S %z')
+df['method'] = df['request'].str.split().str[0]
+df['path'] = df['request'].str.split().str[1]
+print(f"{len(df)} logs where {len(df[df.method == 'GET'])} are GET, {len(df[df.method == 'POST'])} are POST, {len(df[df.method == 'PUT'])} are PUT, {len(df[df.method == 'PATCH'])} are PATCH, and {len(df[df.method == 'DELETE'])} are DELETE")
+print(f"{len(df[(df.method == 'GET') & (df.path == '/status')])} logs where method=GET path=/status")
