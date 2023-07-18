09b1f1fc8eab6c9768942323895602718bc21573
Nahom1927 <yheys898184@gmail.com>
2023-07-19 01:06:29 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""MongoDB storage"""
-import pandas as pd
+""" MongoDB Operations with Python using pymongo """
+from pymongo import MongoClient
 
-df = pd.read_csv('access.log', sep=' ', header=None,
-                 names=['ip', 'dash1', 'dash2', 'time', 'request', 'status', 'size', 'referer', 'user_agent', 'forwarded_for'])
-df['time'] = df['time'].str.replace('[','').str.replace(']','')
-df['time'] = pd.to_datetime(df['time'], format='%d/%b/%Y:%H:%M:%S %z')
-df['method'] = df['request'].str.split().str[0]
-df['path'] = df['request'].str.split().str[1]
-print(f"{len(df)} logs where {len(df[df.method == 'GET'])} are GET, {len(df[df.method == 'POST'])} are POST, {len(df[df.method == 'PUT'])} are PUT, {len(df[df.method == 'PATCH'])} are PATCH, and {len(df[df.method == 'DELETE'])} are DELETE")
-print(f"{len(df[(df.method == 'GET') & (df.path == '/status')])} logs where method=GET path=/status")
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
