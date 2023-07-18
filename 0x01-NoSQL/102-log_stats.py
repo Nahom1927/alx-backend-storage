43b34613c35e19cf600234bdb378fff0c72dd580
Nahom1927 <yheys898184@gmail.com>
2023-07-19 00:46:29 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
-"""Prints statistics from the nginx collection"""
+"""MongoDB storage"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
+    """check for elements in  collection"""
+    client = MongoClient('mongodb://localhost:27017')
+    collection = client.logs.nginx
 
-    print(logs.count_documents({}), 'logs')
-    print('Methods:')
-    method = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for req in method:
-        print(f'\tmethod {req}:', logs.count_documents({'method': req}))
-    print(logs.count_documents({'method': 'GET','path': '/status'}), 'status check')
+    print(f"{collection.estimated_document_count()} logs")
 
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    print('IPs:')
-    ip_counts = logs.aggregate(
-        [ { "$group" :
-                { "_id": "$ip",
-                  "count": { "$sum": 1 }
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+    print("IPs:")
+    pipeline = [
+            {
+                '$group': {
+                    '_id': '$ip',
+                    'count': {'$sum': 1}
                 }
-          }, 
-          { "$sort": {"count": -1} },
-          { "$limit": 10 } ]
-    )
-    for data in ip_counts:
-        print(f"\t{data['_id']}: {data['count']}")
+            },
+            {
+                '$sort': {
+                    'count': -1
+                }
+            },
+            {
+                '$limit': 10
+            }
+    ]
+    result = collection.aggregate(pipeline)
+    for doc in result:
+        print(f"\t{doc['_id']}: {doc['count']}")
